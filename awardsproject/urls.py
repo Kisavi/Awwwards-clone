26459a13e7d9e404b69dc1d04a0597fa85022cac
Kisavi <deniskisavi@gmail.com>
2022-06-11 03:58:38 +0300

    setup login configurations

--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('awardsapp.urls')),
-     path('', include('django.contrib.auth.urls'))
+    # path('', include('awardsapp.urls')),
+    path('', include('django.contrib.auth.urls'))
 ]
