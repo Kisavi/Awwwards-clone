0b624e2f5ef10c93c7a2fac679c16540d9d52ec4
Kisavi <deniskisavi@gmail.com>
2022-06-13 18:57:18 +0300

    define a form class to be used for posting a project

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Profile, Project
 
 
 class RegisterForm(UserCreationForm):
@@ -19,3 +19,15 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['photo', 'bio']
+
+
+class PostProject(forms.ModelForm):
+    name = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    link = forms.URLField(widget=forms.URLField(attrs={'class': 'form-control'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    category = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Project
+        fields = ['category', 'name', 'description', 'link', 'image']
