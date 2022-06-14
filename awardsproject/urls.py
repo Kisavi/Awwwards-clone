6986553bf245d4d30ab053e58ccafe4848a2e259
Kisavi <deniskisavi@gmail.com>
2022-06-14 23:25:16 +0300

    add star_ratings url in my project urls

--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('awardsapp.urls')),
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
 ]
